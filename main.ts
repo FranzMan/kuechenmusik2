abc0db2175821160e003923ff0a9dca8f363e921
FranzMan <thefranzman@googlemail.com>
2021-07-20 14:04:10 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onPinPressed(TouchPin.P0, function () {
+    music.playTone(196, music.beat(BeatFraction.Quarter))
+})
+input.onPinPressed(TouchPin.P3, function () {
+    music.playTone(277, music.beat(BeatFraction.Quarter))
+})
+input.onPinPressed(TouchPin.P2, function () {
+    music.playTone(262, music.beat(BeatFraction.Quarter))
+})
+input.onPinPressed(TouchPin.P1, function () {
+    music.playTone(233, music.beat(BeatFraction.Quarter))
+})
